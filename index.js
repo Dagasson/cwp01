aba3b9cc2b42063728222ceceba33e9285cd2c70
Unknown <mushuk-artur@mail.ru>
2017-10-19 17:50:04 +0300

    Created summary

--- index.js
+++ index.js
@@ -1,4 +1,35 @@
-for(var i=2; i<process.argv.length; i++){
-var name = process.argv[i];
-console.log(`Hi ${name}!`);
+const fs = require('fs');
+const path=require('path');
+let pat= process.argv[2];
+let copyright;
+
+copyright=JSON.parse(fs.readFileSync("./cop.json"));
+
+let script='console.log("Kekus"); \n'+
+    'console.log("double kekus");'
+
+
+if(pat)
+{
+	fs.stat(pat,function (error, statistics) {
+		if(error){console.log('Ошибка пути');}
+		else{
+			//watchDir(pat)
+
+                fs.writeFile(pat+'\\Summary.js', script, function (error) {
+                        if (error) console.log('Ошибка создания файла.');
+                        else console.log('Summary создан.');
+                    }
+                );
+
+		}
+    })
 }
+
+
+
+//let content=fs.readFile(`${path}`,"utf8",function(error, data)
+//	{
+  //      console.log(data)
+ //   }
+//);
